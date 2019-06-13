860ae10d41943210212503d8b6d71a4ff843fefb
Mike <mewajda@gmail.com>
2019-06-12 23:00:33 -0500

    feat: add payment information validation messages

--- Nixtus.Plugin.Payments.Nmi/Components/NmiViewComponent.cs
+++ Nixtus.Plugin.Payments.Nmi/Components/NmiViewComponent.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Nixtus.Plugin.Payments.Nmi.Models;
 using Nop.Web.Framework.Components;
 
@@ -11,14 +10,6 @@ namespace Nixtus.Plugin.Payments.Nmi.Components
         public IViewComponentResult Invoke()
         {
             var model = new PaymentInfoModel();
-
-            //set postback values (we cannot access "Form" with "GET" requests)
-            if (Request.Method == WebRequestMethods.Http.Get)
-                return View("~/Plugins/Payments.Nmi/Views/PaymentInfo.cshtml", model);
-
-            var form = Request.Form;
-            model.CardNumber = form["CardNumber"];
-            model.CardCode = form["CardCode"];
 
             return View("~/Plugins/Payments.Nmi/Views/PaymentInfo.cshtml", model);
         }
